a1f5e2890e457f721115d4b171f6ee265ffa390b
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-18 23:46:29 +0530

    adas

--- testapp1/src/userform.js
+++ testapp1/src/userform.js
@@ -16,7 +16,7 @@ const userform = props => {
         >
             <Grid item
                     xs={12}>
-                <Typography component={'h1'}></Typography>
+                <Typography component={'h1'} sx={{color:'#000000'}}></Typography>
 
 
             </Grid>
