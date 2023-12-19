e78f06845de0d825fcd83590904ecd6e8920ca5c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 00:26:29 +0530

    asd

--- nvd-backend/server.js
+++ nvd-backend/server.js
@@ -1,4 +1,7 @@
 const app = require('./app');
+//variables
+const port =3001;
+
 
 const server = app.listen(3001,'127.0.0.1',()=>{
     console.log('Node server is listenging to ${server.address().port} ')
