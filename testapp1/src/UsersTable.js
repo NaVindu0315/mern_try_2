8d82cdbe9b9d964cbe5f5d16214b99c13e5b24e7
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-19 21:29:02 +0530

    sad

--- testapp1/src/UsersTable.js
+++ testapp1/src/UsersTable.js
@@ -54,7 +54,7 @@ const UsersTable = ({rows}) =>
                             )
                             ):
                             (
-                                <TableRow key={row.id} sx ={{'&:last-child td,&:last-child th':{border:0}}}>
+                                <TableRow sx ={{'&:last-child td,&:last-child th':{border:0}}}>
 
 <TableCell component='th' scope="row">No Data</TableCell> 
                                 </TableRow>
