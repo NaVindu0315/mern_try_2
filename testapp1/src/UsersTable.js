4da4caff1f893d0426a250fbdfb6adca547c5663
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-19 21:26:03 +0530

    sa

--- testapp1/src/UsersTable.js
+++ testapp1/src/UsersTable.js
@@ -20,7 +20,7 @@ const UsersTable = ({rows}) =>
                 </TableHead>
                 <TableBody>
                     {
-                        rows.map(row => 
+                        rows.length > 0 ?  rows.map(row => 
                             (
                             <TableRow key={row.id} sx ={{'&:last-child td,&:last-child th':{border:0}}}>
                              <TableCell component='th' scope="row">(row.id)</TableCell> 
@@ -52,7 +52,11 @@ const UsersTable = ({rows}) =>
                             </TableRow>
                                
                             )
+                            ):
+                            (
+
                             )
+
                     }
 
 
