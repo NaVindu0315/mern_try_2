d200e51885f6160269bc9394faf8ddce46d9bce4
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-19 19:49:56 +0530

    sada

--- testapp1/src/UsersTable.js
+++ testapp1/src/UsersTable.js
@@ -23,9 +23,7 @@ const UsersTable = ({rows}) =>
                         rows.map(row => 
                             (
                                 <TableRow key={row.id} sx ={{'&:last-child td,&:last-child th':{border:0}}}
-                                sx={{
-
-                                }}
+                               
                                 >
                                      <TableCell component='th' scope="row">
                                     (row.id)
