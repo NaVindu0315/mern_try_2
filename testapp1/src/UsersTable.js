e6e5cdd91ca2e0f7fb558f520b6280f6db5a7435
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-19 14:50:25 +0530

    sa

--- testapp1/src/UsersTable.js
+++ testapp1/src/UsersTable.js
@@ -1,7 +1,17 @@
 
-import { TableContainer } from "@mui/material";
+import { TableContainer, TableHead, TableRow } from "@mui/material";
 const UsersTable = props =>
 {
+<TableContainer 
+        component={paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        
+                    </TableRow>
+                </TableHead>
+            </Table>
 
+</TableContainer>
 }
 export default UsersTable;
