9cc77c80e60cb94ca8b8bdbe91a74e1ed224e8c1
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 14:00:56 +0530

    sda

--- nvd-backend/controller.js
+++ nvd-backend/controller.js
@@ -46,7 +46,14 @@ const updateUser  = (req,res,next)=>
     const {id,name} = req.body;
     User.updateOne({id:id}, { $set : {name :name}
     })
-    
+    .then(response=>
+        {
+            res.json({response})
+        })
+        .catch(error=>{
+            res.json({error})
+        });
+
 }
 
 
