bd80edd0536f4854c0455035f25d2aaaa81a39a7
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 20:16:24 +0530

    asdad

--- nvd-backend/router.js
+++ nvd-backend/router.js
@@ -6,9 +6,9 @@ const controller = require('./controller');
 //creating routes for each
 
 router.get('/users',controller.getUsers);
-router.get('/createuser',controller.addUser);
-router.get('/updateuser',controller.updateUser);
-router.get('/deleteuser',controller.deleteUser);
+router.post('/createuser',controller.addUser);
+router.post('/updateuser',controller.updateUser);
+router.post('/deleteuser',controller.deleteUser);
 
 
 module.exports = router;
