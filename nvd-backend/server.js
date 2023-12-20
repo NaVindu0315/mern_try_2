ce1741a514e3acaad715b5df7d3240b675c29df8
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 12:56:35 +0530

    sad

--- nvd-backend/server.js
+++ nvd-backend/server.js
@@ -27,5 +27,5 @@ const connect = async() => {
 connect();
 
 const server = app.listen(port,host,()=>{
-    console.log('Node server is listenging to ${server.address().port} ');
+    console.log('Node server is listenging to ',server.address().port);
 });
