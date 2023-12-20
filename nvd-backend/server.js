c83cfc68cd2e773c3df6dcf8703ff39d105c5dab
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 12:21:55 +0530

    sda

--- nvd-backend/server.js
+++ nvd-backend/server.js
@@ -3,6 +3,9 @@ const app = require('./app');
 const port =3001;
 const host ='127.0.0.1';
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
 const server = app.listen(port,host,()=>{
     console.log('Node server is listenging to ${server.address().port} ');
