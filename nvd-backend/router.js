5941215145b6780d9f0a8e675aa26d41b1829773
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 19:51:25 +0530

    exports the router

--- nvd-backend/router.js
+++ nvd-backend/router.js
@@ -9,3 +9,6 @@ router.get('/users',controller.getUsers);
 router.get('/createuser',controller.addUser);
 router.get('/updateuser',controller.updateUser);
 router.get('/deleteuser',controller.deleteUser);
+
+
+module.exports = router;
