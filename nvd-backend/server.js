5c701d7890702f0bc12c6b824253ba80ef7d6508
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-20 12:43:05 +0530

    sad

--- nvd-backend/server.js
+++ nvd-backend/server.js
@@ -11,6 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 const uri ="mongodb+srv://navindu0315:Navindu99@cluster0.do0vonn.mongodb.net/?retryWrites=true&w=majority"
+const connect = async() => {
+    try 
+    {
+
+    }
+    catch{
+        
+    }
+}
 
 const server = app.listen(port,host,()=>{
     console.log('Node server is listenging to ${server.address().port} ');
