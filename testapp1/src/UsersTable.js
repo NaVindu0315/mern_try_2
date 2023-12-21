3aa5529d97877fe07344059ecd2079e52e8b61c8
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-21 11:57:31 +0530

    working user table

--- testapp1/src/UsersTable.js
+++ testapp1/src/UsersTable.js
@@ -1,8 +1,8 @@
 
 import { Paper, TableCell, TableContainer, TableHead, TableRow , TableBody, Button, Table} from "@mui/material";
-const IdCell = ({ row }) => <TableCell>{row.id}</TableCell>;
+//const IdCell = ({ row }) => <TableCell>{row.id}</TableCell>;
 
-const NameCell = ({ row }) => <TableCell>{row.name}</TableCell>;
+//const NameCell = ({ row }) => <TableCell>{row.name}</TableCell>;
 const UsersTable = ({rows}) =>
 {
     return(
@@ -27,8 +27,8 @@ const UsersTable = ({rows}) =>
                         rows.length > 0 ?  rows.map((row) => 
                             (
                             <TableRow key={row.id} sx ={{'&:last-child td,&:last-child th':{border:0}}}>
-                             <IdCell row={row} />
-                            <NameCell row={row} />
+                            <TableCell component='th' scope="row">{row.id}</TableCell>
+                            <TableCell component='th' scope="row">{row.name}</TableCell>
                              <TableCell>
                                 <Button sx={{margin:'0px 10px'}}
                                 onClick={
