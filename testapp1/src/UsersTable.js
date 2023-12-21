e14b23c6681870397cfae2fec1ce135c39e7a75a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-21 20:00:52 +0530

    qwqsad

--- testapp1/src/UsersTable.js
+++ testapp1/src/UsersTable.js
@@ -46,6 +46,7 @@ const UsersTable = ({rows, selectedUser}) =>
                                 onClick={
                                     () =>
                                     {
+                                        deleteUser({id:row.id})
 
                                     }
                                 }
