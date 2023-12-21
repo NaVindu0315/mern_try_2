880a7e4e7fcfb5793149a5a33c10734a818129d6
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-21 11:07:43 +0530

    asda

--- testapp1/src/user.js
+++ testapp1/src/user.js
@@ -5,21 +5,7 @@ import UsersTable from "./UsersTable";
 //import for connecting backend and front end
 import Axios from "axios";
 
-const users = [
-    {
-        id:1,
-        name:'Navindu',
-    },
-    {
-        id:2,
-        name:'pakaya',
-    },
-    {
-        id:3,
-        name:'hutti',
-    }
 
-];
 const Users =() =>
 {
 return(
