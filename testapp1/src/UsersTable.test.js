2f0942d28b9a8d5490c3f3d8d9e8f9869a29da48
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-30 14:05:14 +0530

    added usertable.test.js

--- testapp1/src/UsersTable.test.js
+++ testapp1/src/UsersTable.test.js
@@ -1,9 +1,20 @@
-import UsersTable from "./UsersTable";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import UsersTable from './UsersTable';
 
-describe(UsersTable, () => {
-  it("should render correctly", () => {
-    const component = shallow(<UsersTable />);
+test('renders the table with data', () => {
+  const rows = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+  ];
+  const selectedUser = jest.fn();
+  const deleteUser = jest.fn();
 
-    expect(component).toMatchSnapshot();
-  });
+  render(<UsersTable rows={rows} selectedUser={selectedUser} deleteUser={deleteUser} />);
+
+  expect(screen.getByText('ID')).toBeInTheDocument();
+  expect(screen.getByText('Name')).toBeInTheDocument();
+  expect(screen.getByText('Actions')).toBeInTheDocument();
+  expect(screen.getAllByRole('cell', { name: /Alice/i })).toHaveLength(2);
+  expect(screen.getAllByRole('cell', { name: /Bob/i })).toHaveLength(2);
 });
