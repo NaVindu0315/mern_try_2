60c0451a80ab67eb0fe6b01d428b02ac12419d81
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-30 14:10:03 +0530

    tests are not working

--- testapp1/src/UsersTable.test.js
+++ testapp1/src/UsersTable.test.js
@@ -18,3 +18,27 @@ test('renders the table with data', () => {
   expect(screen.getAllByRole('cell', { name: /Alice/i })).toHaveLength(2);
   expect(screen.getAllByRole('cell', { name: /Bob/i })).toHaveLength(2);
 });
+
+test('renders a message when there is no data', () => {
+    const rows = [];
+    const selectedUser = jest.fn();
+    const deleteUser = jest.fn();
+  
+    render(<UsersTable rows={rows} selectedUser={selectedUser} deleteUser={deleteUser} />);
+  
+    expect(screen.getByText('No Data')).toBeInTheDocument();
+  });
+
+  test('calls the selectedUser function when a user is selected', () => {
+    const rows = [{ id: 1, name: 'Alice' }];
+    const selectedUser = jest.fn();
+    const deleteUser = jest.fn();
+  
+    render(<UsersTable rows={rows} selectedUser={selectedUser} deleteUser={deleteUser} />);
+  
+    const updateButton = screen.getByRole('button', { name: /Update/i });
+    fireEvent.click(updateButton);
+  
+    expect(selectedUser).toHaveBeenCalledWith({ id: 1, name: 'Alice' });
+  });
+  
