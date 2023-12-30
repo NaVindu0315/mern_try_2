b3cea57c0f267e775fbf28969ecfb4a5ed270af0
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-30 20:02:20 +0530

    added new tests

--- nvd-backend/Test/index.test.js
+++ nvd-backend/Test/index.test.js
@@ -5,3 +5,11 @@ const { add } = require('../index');
 test('toBe', () => {
     expect(add(1, 2)).toBe(3);
 });
+
+test('toEqual', () => {
+    expect(add(1, 2)).toEqual(3);
+});
+
+test('tobenull', () => {
+    expect(add(1, 2)).not.toBeNull();
+}); 
